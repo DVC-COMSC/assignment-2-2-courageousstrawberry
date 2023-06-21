3c9048510d0ff3fcde4ad84b7420924a11cf262e
Eleni <elech2a@gmail.com>
2023-06-21 10:56:42 -0700

    Celcius to Fahrenheit

--- main.py
+++ main.py
@@ -11,8 +11,12 @@ def main():
     # Do not delete the return statement
     ########################################
     """
-    return celcius, fahrenheit
+    celcius = float(input('Enter a temperature in Celcius: '))
+    fahrenheit = (9*celcius)/5 + 32
 
+    print(f'The temperature in fahrenheit is: {fahrenheit:.2f}')
+
+    return celcius, fahrenheit
 
 if __name__ == '__main__':
     main()
